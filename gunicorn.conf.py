4cbc1ba8cb1314271866f57ef5086b7be840a8ad
root <root@Caddy-server.foj0nyihxbwetbqhcy23rtrj3g.ux.internal.cloudapp.net>
2024-05-17 03:16:04 +0000

    Add timestamp & address

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,8 @@
 bind = "127.0.0.1:8000" # Choose a free port, it doesn't have to be 5000
 workers = 4 # Adjust this based on your VM resources; a good starting point is 2-4 times the number of CPU cores. 
 timeout = 120 # Adjust as needed
+
+# Logging configuration
+accesslog = "-" # Log to standard output
+errorlog = "-"  # Log errors to standard output
+access_log_format = '%(t)s %(h)s %(l)s %(u)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
